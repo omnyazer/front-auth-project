e3b35f242cae4c47d97dacca19068d05a9635f9f
omnya zerghouni <omnyazer@gmail.com>
2025-05-14 14:53:52 +0200

    activité 11

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,46 +5,38 @@ import "../assets/styles/Header.css";
 
 function Header() {
   const location = useLocation();
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isConnected, setIsConnected] = useState(false);
 
-  const checkAuth = () => {
+  const checkToken = () => {
     const auth = JSON.parse(localStorage.getItem("auth"));
-    return auth && new Date(auth.expiresAt) > new Date();
+    const isValid = auth && new Date(auth.expiresAt) > new Date();
+    return isValid;
   };
 
   useEffect(() => {
-    setIsLoggedIn(checkAuth());
+    setIsConnected(checkToken());
   }, [location]);
 
   return (
     <Navbar bg="light" data-bs-theme="light">
       <Container>
         <Nav className="ms-auto">
-          <Nav.Link as={NavLink} to="/">
-            Accueil
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/offres/publiques">
-            Offres Publiques
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/offres/professionnelles">
-            Offres Professionnelles
-          </Nav.Link>
+          <Nav.Link as={NavLink} to="/">Accueil</Nav.Link>
+          <Nav.Link as={NavLink} to="/offres/publiques">Offres Publiques</Nav.Link>
 
-          {!isLoggedIn && (
-            <>
-              <Nav.Link as={NavLink} to="/inscription">
-                Inscription
-              </Nav.Link>
-              <Nav.Link as={NavLink} to="/connexion">
-                Connexion
-              </Nav.Link>
-            </>
+          {isConnected && (
+            <Nav.Link as={NavLink} to="/offres/professionnelles">
+              Offres Professionnelles
+            </Nav.Link>
           )}
 
-          {isLoggedIn && (
-            <Nav.Link as={NavLink} to="/deconnexion">
-              Déconnexion
-            </Nav.Link>
+          {!isConnected ? (
+            <>
+              <Nav.Link as={NavLink} to="/inscription">Inscription</Nav.Link>
+              <Nav.Link as={NavLink} to="/connexion">Connexion</Nav.Link>
+            </>
+          ) : (
+            <Nav.Link as={NavLink} to="/deconnexion">Déconnexion</Nav.Link>
           )}
         </Nav>
       </Container>
