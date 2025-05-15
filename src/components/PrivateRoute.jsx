2bc880a0227312ea3ef48cfa9e856f4ab5774046
omnya zerghouni <omnyazer@gmail.com>
2025-05-15 14:28:54 +0200

    correction fichier PrivateRoute

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,18 +1,25 @@
 import { useNavigate, Outlet } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 const PrivateRoute = () => {
   const navigate = useNavigate();
   const auth = useSelector((state) => state.auth);
+  const [checking, setChecking] = useState(true);
 
   useEffect(() => {
     const isValid = auth.token && new Date(auth.expiresAt) > new Date();
 
     if (!isValid) {
       navigate("/connexion");
+    } else {
+      setChecking(false);
     }
   }, [auth, navigate]);
+
+  if (checking) {
+    return null;
+  }
 
   return <Outlet />;
 };
